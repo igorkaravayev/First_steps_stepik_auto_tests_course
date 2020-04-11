b88ddb7fa5808e1e0553e1054b531523a4a5bbbe
igorkaravayev <62888986+igorkaravayev@users.noreply.github.com>
2020-04-11 17:57:48 +0300

    First commit Lesson_2_4_2

--- Lesson_2_4_2.py
+++ Lesson_2_4_2.py
@@ -5,9 +5,7 @@ from selenium import webdriver
 import math
 
 browser = webdriver.Chrome()
-
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
-
 WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
 button = browser.find_element_by_id("book")
 button.click()
@@ -21,6 +19,3 @@ element = browser.find_element_by_css_selector("#answer")
 element.send_keys(y)
 button1 = browser.find_element_by_css_selector("#solve")
 button1.click()
-#message = browser.find_element_by_id("verify_message")
-
-#assert "successful" in message.text
